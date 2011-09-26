b90e8d38e540947c5fd3aafa8939498222fbbfc5
David Puebla Garcia <david.puebla@gmail.com>
2011-09-25 22:01:45 -0400

    After exercise

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -26,5 +26,15 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+  end
 end
 
